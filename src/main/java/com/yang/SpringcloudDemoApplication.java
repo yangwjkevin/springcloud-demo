7547b3bd8ecb52f6888815f8a644b9e25b39d707
yangwenjia <yangwenjia@zhibankeji.com>
2020-07-09 15:40:29 +0800

    Initial commit

---
+++ src/main/java/com/yang/SpringcloudDemoApplication.java
@@ -0,0 +1,15 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudDemoApplication.class, args);
+	}
+
+}
